f6df728416b45aaf445ff2455e2c88106f754a95
weitweety <weitweety@gmail.com>
2018-04-29 23:02:00 +0800

    Update checkupdate.js

--- checkupdate.js
+++ checkupdate.js
@@ -4,15 +4,15 @@ $(function(){
 })
 function getalarm (){
  $.ajax({
-   type: 'GET',                     //GET or POST
-   url: "https://postwallapp.herokuapp.com/updatepic.php",  //請求的頁面
-   cache: false,   //是否使用快取
+   type: 'GET',                    
+   url: "https://postwallapp.herokuapp.com/updatepic.php",  
+   cache: false,   
    data: {pinnum: pin},
-   success: function(result){   //處理回傳成功事件，當請求成功後此事件會被呼叫
+   success: function(result){   
     //alert("success, pin is" + pin);
   $('#printpic').html(result);
    },
-   error: function(result){   //處理回傳錯誤事件，當請求失敗後此事件會被呼叫
+   error: function(result){   
   //alert("error, pin is" + pin);
   console.log(result);
    },
